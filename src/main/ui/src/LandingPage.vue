c7cb6bb23593b6c6fa7647c1c9f688acda3ca710
David Csida <david.csida@tum.de>
2022-06-16 09:47:06 +0200

    landing page now respects browser dark/light theme

--- src/main/ui/src/LandingPage.vue
+++ src/main/ui/src/LandingPage.vue
@@ -6,8 +6,18 @@
 <w-app>
     <w-flex grow basis-zero wrap>
         <w-flex class="grow column align-center justify-center">
-            <h1 style="color:#020202; text-align: center; padding: 12px;">Welcome to Garching Airlines</h1>
-            <h2 style="color:#020202; text-align: center;">Flights of Excellence</h2>
+            <h1 style="color:#020202; text-align: center;">Welcome to Garching Airlines</h1>
+            <h3 style="color:#020202; text-align: center;">Flights of Excellence</h3>
+
+            <w-grid columns="3" class="card-wrapper">
+                <w-card>sample text</w-card>
+                <w-card>sample text</w-card>
+                <w-card>sample text</w-card>
+                <w-card>sample text</w-card>
+                <w-card>sample text</w-card>
+                <w-card>sample text</w-card>
+            </w-grid>
+
         </w-flex>
     </w-flex>
 </w-app>
