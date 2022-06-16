cf45b710710f9dfb243f6ed91a6d4024db66dfea
David Csida <david.csida@tum.de>
2022-06-16 10:59:05 +0200

    added dummy buttons

--- src/main/ui/src/LandingPage.vue
+++ src/main/ui/src/LandingPage.vue
@@ -6,17 +6,24 @@
 <w-app>
     <w-flex grow basis-zero wrap>
         <w-flex class="grow column align-center justify-center">
-            <h1 style="color:#020202; text-align: center;">Welcome to Garching Airlines</h1>
-            <h3 style="color:#020202; text-align: center;">Flights of Excellence</h3>
 
-            <w-grid columns="3" class="card-wrapper">
-                <w-card>sample text</w-card>
-                <w-card>sample text</w-card>
-                <w-card>sample text</w-card>
-                <w-card>sample text</w-card>
-                <w-card>sample text</w-card>
-                <w-card>sample text</w-card>
+            <div class="spacer"></div>
+
+            <h1 style="text-align: center;">Welcome to Garching Airlines</h1>
+            <h3 style="text-align: center;">Flights of Excellence</h3>
+
+            <w-grid columns="4" class="card-wrapper">
+                <w-button>Flights</w-button>
+                <w-button>Catering</w-button>
+                <w-button>About my destination</w-button>
+                <w-button>About my trip</w-button>
             </w-grid>
+
+            <div class="spacer"></div>
+
+            <w-button class="survey-button align-self-end">
+                We value your opinion
+            </w-button>
 
         </w-flex>
     </w-flex>
