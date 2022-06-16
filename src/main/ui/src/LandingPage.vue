7c0f218dc411bcc61cb5f65f9a421cad95491c25
David Csida <david.csida@tum.de>
2022-06-16 09:05:46 +0200

    migrated css from landing page to css file

--- src/main/ui/src/LandingPage.vue
+++ src/main/ui/src/LandingPage.vue
@@ -5,7 +5,7 @@
 <template>
 <w-app>
     <w-flex grow basis-zero wrap>
-        <w-flex class="grow mx1 column align-center justify-center">
+        <w-flex class="grow column align-center justify-center">
             <h1 style="color:#020202; text-align: center; padding: 12px;">Welcome to Garching Airlines</h1>
             <h2 style="color:#020202; text-align: center;">Flights of Excellence</h2>
         </w-flex>
@@ -15,13 +15,6 @@
 
 <style>
 @import "./assets/css/base.css";
-
-body {
-    height: 100vh;
-    width: 100vw;
-    background: url("./assets/img/above_clouds.jpg") center center fixed no-repeat;
-    background-size: cover;
-}
-
+@import "./assets/css/landing_page.css";
 
 </style>
