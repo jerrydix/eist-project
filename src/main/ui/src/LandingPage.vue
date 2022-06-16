f58970ef0850aaef8f055fc2a7b8e1b787f21996
David Csida <david.csida@tum.de>
2022-06-16 08:58:01 +0200

    centered sample text

--- src/main/ui/src/LandingPage.vue
+++ src/main/ui/src/LandingPage.vue
@@ -4,7 +4,12 @@
 
 <template>
 <w-app>
-<h1 style="color:#020202">Welcome to Garching Airlines</h1>
+    <w-flex grow basis-zero wrap>
+        <w-flex class="grow mx1 column align-center justify-center">
+            <h1 style="color:#020202; text-align: center; padding: 12px;">Welcome to Garching Airlines</h1>
+            <h2 style="color:#020202; text-align: center;">Flights of Excellence</h2>
+        </w-flex>
+    </w-flex>
 </w-app>
 </template>
 
@@ -12,6 +17,8 @@
 @import "./assets/css/base.css";
 
 body {
+    height: 100vh;
+    width: 100vw;
     background: url("./assets/img/above_clouds.jpg") center center fixed no-repeat;
     background-size: cover;
 }
