ee41e2a8ceb3b93c4bca0149bbce7c0f2e255238
GeorgeMer <georgemerezas@gmail.com>
2022-06-26 10:58:05 +0200

    Small changes to Home

--- src/main/ui/src/components/HomeLoginButton.vue
+++ src/main/ui/src/components/HomeLoginButton.vue
@@ -1,45 +1,56 @@
 <script>
-import { userStore } from "../userStore.js";
-import { logout } from "../services/UserService.js";
+import {userStore} from "../userStore.js";
+import {logout} from "../services/UserService.js";
 /* import Login from "./User/Login.vue";
  */ //import "../assets/css/home.css";
 export default {
-	setup() {
-		const store = userStore();
-		return {
-			store,
-		};
-	},
-	methods: {
-		logoutUser() {
-			logout(this.store.username).then((response) => {
-				if (
-					!(response === "Broke the system") &&
-					!(response === "Error")
-				) {
-					window.localStorage.removeItem("user");
-					this.store.username = null;
-				}
-				console.log(response);
-			});
-		},
-	},
-	data: () => ({
-		showDialog: false,
-	}),
+  setup() {
+    const store = userStore();
+    return {
+      store,
+    };
+  },
+  methods: {
+    logoutUser() {
+      logout(this.store.username).then((response) => {
+        if (
+            !(response === "Broke the system") &&
+            !(response === "Error")
+        ) {
+          window.localStorage.removeItem("user");
+          this.store.username = null;
+        }
+        console.log(response);
+      });
+    },
+  },
+  data: () => ({
+    showDialog: false,
+  }),
 };
 </script>
 
 <template>
-	<div v-if="!this.store.username" class="align-self-end">
-		<RouterLink to="/register">
-			<w-button class="nav-button">Register</w-button>
-		</RouterLink>
-		<RouterLink to="/login">
-			<w-button class="nav-button">Login</w-button>
-		</RouterLink>
-	</div>
-	<div v-if="this.store.username" class="align-self-end">
-		<w-button class="nav-button" @click="logoutUser"> Logout </w-button>
-	</div>
+  <w-flex class="text-center">
+
+    <w-flex align-self-start class="xs11 pa10">
+      <h1 v-if="this.store.username">Welcome back, {{ this.store.username }}</h1>
+    </w-flex>
+
+
+    <w-flex v-if="!this.store.username" align-self-end class="xs1 pa0">
+      <RouterLink to="/register">
+        <w-button class="nav-button">Register</w-button>
+      </RouterLink>
+      <RouterLink to="/login">
+        <w-button class="nav-button">Login</w-button>
+      </RouterLink>
+    </w-flex>
+
+    <w-flex v-if="this.store.username" align-self-end class="xs1 pa1">
+      <w-button class="nav-button" @click="logoutUser"> Logout</w-button>
+    </w-flex>
+
+
+  </w-flex>
 </template>
--- src/main/ui/src/views/Home.vue
+++ src/main/ui/src/views/Home.vue
@@ -6,76 +6,75 @@ import FlightInfo from "../components/FlightInfo.vue";
 import Login from "./User/Login.vue";
 //import "../assets/css/home.css";
 export default {
-	components: {
-		HomeNav,
-		HomeLoginButton,
-		FlightInfo,
-		Login,
-	},
+  components: {
+    HomeNav,
+    HomeLoginButton,
+    FlightInfo,
+    Login,
+  },
 
-	data: () => ({
-		showLoginDialog: false,
-	}),
+  data: () => ({
+    showLoginDialog: false,
+  }),
 };
 </script>
 
 <template>
-	<w-app>
-		<w-flex basis-zero grow wrap>
-			<w-flex class="grow column align-center justify-center">
-				
-        <HomeLoginButton />
+  <w-app>
+    <HomeLoginButton/>
+    <w-flex basis-zero grow wrap>
 
-				<div class="spacer"></div>
+      <w-flex class="grow column align-center justify-center">
 
-				<h1>Welcome to Garching Airlines</h1>
-				<h3>Flights of Excellence</h3>
 
-				<HomeNav />
+        <div class="spacer"></div>
 
-				<FlightInfo />
+        <h1>Welcome to Garching Airlines</h1>
+        <h3>Flights of Excellence</h3>
 
-				<w-button class="px4" @click="showLoginDialog = true">
-					Open dialog
-				</w-button>
+        <HomeNav/>
 
-				<w-dialog v-model="showLoginDialog" title="Login">
-					<Login />
-				</w-dialog>
+        <FlightInfo/>
 
-				<div class="spacer"></div>
+        <w-button class="px4" @click="showLoginDialog = true">
+          Open dialog
+        </w-button>
 
-				<div class="align-self-end">
-					<RouterLink to="/survey">
-						<w-button class="survey-button">
-							We value your opinion
-						</w-button>
-					</RouterLink>
-				</div>
-			</w-flex>
-		</w-flex>
-	</w-app>
+        <w-dialog v-model="showLoginDialog" title="Login">
+          <Login/>
+        </w-dialog>
+
+        <div class="spacer"></div>
+
+        <div class="align-self-end">
+          <RouterLink to="/survey">
+            <w-button class="survey-button">
+              We value your opinion
+            </w-button>
+          </RouterLink>
+        </div>
+      </w-flex>
+    </w-flex>
+  </w-app>
 </template>
 
 <style scoped>
 @import "../assets/css/home.css";
 
 @media (prefers-color-scheme: light) {
-	.w-app {
-		background: url("../assets/img/above_clouds.jpg") center center fixed
-			no-repeat !important;
-	}
+  .w-app {
+    background: url("../assets/img/above_clouds.jpg") center center fixed no-repeat !important;
+  }
 }
 
 @media (prefers-color-scheme: dark) {
-	.w-app {
-		background: url("../assets/img/above_clouds_dark.jpg") center center
-			fixed no-repeat !important;
-	}
+  .w-app {
+    background: url("../assets/img/above_clouds_dark.jpg") center center fixed no-repeat !important;
+  }
 }
 
 .w-app {
-	background-size: cover !important;
-	text-align: center;
+  background-size: cover !important;
+  text-align: center;
 }
 </style>
