7b868709fabbd6e4eeac16eab9387349f34cfce6
GeorgeMer <georgemerezas@gmail.com>
2022-06-28 21:37:51 +0200

    Merge

---
+++ src/main/ui/src/views/User/UserDashboard.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+  
+<script>
+import {userStore} from "../../userStore";
+
+export default {
+  setup() {
+    const store = userStore();
+    return {
+      store,
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
