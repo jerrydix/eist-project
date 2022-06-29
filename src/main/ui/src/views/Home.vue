afe5421e7049ea0eff76c38fdbe1adf56e352caa
David Csida <david.csida@tum.de>
2022-06-29 12:40:34 +0200

    really solved conflict this time

--- src/main/ui/src/views/Home.vue
+++ src/main/ui/src/views/Home.vue
@@ -28,37 +28,6 @@ export default {
 </script>
 
 <template>
-<<<<<<< HEAD
-  <w-app>
-    <w-toolbar>
-      <RouterLink to="/flights">
-        <w-button>Flights</w-button>
-      </RouterLink>
-      <RouterLink to="/catering">
-        <w-button>Catering</w-button>
-      </RouterLink>
-      <RouterLink to="/poi">
-        <w-button>About my destination</w-button>
-      </RouterLink>
-      <div class="spacer"></div>
-      <!-- <span class="ml2">Item 1</span> -->
-      <div class="ml2">
-        <w-button class="px4" @click="showRegisterDialog = true">
-          Register
-        </w-button>
-      </div>
-      <div class="ml2">
-        <w-button class="px4" @click="showLoginDialog = true">
-          Login
-        </w-button>
-      </div>
-    </w-toolbar>
-    <w-flex basis-zero grow wrap>
-      <w-flex class="grow column align-center justify-center">
-        <div class="top-wrapper">
-          <WelcomeMessage/>
-        </div>
-=======
 	<w-app>
 		<!-- <w-toolbar>
 			<RouterLink to="/flights">
@@ -90,12 +59,10 @@ export default {
 				<div class="top-wrapper">
 					<WelcomeMessage />
 				</div>
->>>>>>> 914ed0b (made own component for loginbuttons)
 
 
         
 
-<<<<<<< HEAD
         <div class="xs4">
           <h1>Welcome to Garching Airlines</h1>
           <h3><em>Flights of Excellence</em></h3>
@@ -103,19 +70,6 @@ export default {
           <FlightInfo/>
         </div>
 
-=======
-				<!-- <w-dialog
-					v-model="showRegisterDialog"
-					title="Register"
-					:width="550"
-				>
-					<Register />
-				</w-dialog>
-
-				<w-dialog v-model="showLoginDialog" title="Login" :width="550">
-					<Login />
-				</w-dialog> -->
->>>>>>> 914ed0b (made own component for loginbuttons)
 
         <w-dialog
             v-model="showRegisterDialog"
