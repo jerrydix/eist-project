6200ee72822893db1a63264f78da25256c824ef6
David Csida <david.csida@tum.de>
2022-06-29 17:21:34 +0200

    placed google api key in .env file so that it won't be exposed

--- src/main/ui/src/views/POI.vue
+++ src/main/ui/src/views/POI.vue
@@ -1,8 +1,7 @@
 <template>
-	<p>{{ key }}</p>
 	<GoogleMap
-		api-key="AIzaSyBKiScI4WumTVipTbFuC6KPHic3dC66tvM"
-		style="width: 100%; height: 600px"
+		:api-key=key
+		style="width: 100vw; height: 100vh"
 		:center="center"
 		:zoom="15"
 	>
@@ -20,10 +19,8 @@ export default defineComponent({
 	setup() {
 		const center = { lat: 40.689247, lng: -74.044502 };
 		const excellence = { lat: 48.2650433, lng: 11.6693806 };
-		return { center, excellence };
+        const key = import.meta.env.VITE_GOOGLE_API_KEY;
+		return { key, center, excellence };
 	},
-	data: () => ({
-		key: import.meta.VUE_APP_GOOGLE_API_KEY,
-	}),
 });
 </script>
