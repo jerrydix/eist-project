f284bb57cc0ea5a3a4c9b671caf90c1c0ad0ba15
GeorgeMer <georgemerezas@gmail.com>
2022-06-29 17:33:23 +0200

    Flight Service that gives back city suggestions and also gets departure and arrival cities and a date and returns all flights with those specifications

--- src/main/java/server/rest/FlightRest.java
+++ src/main/java/server/rest/FlightRest.java
@@ -1,12 +1,15 @@
 package server.rest;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.model.flights.Flight;
 import server.service.FlightService;
+
+import java.util.List;
 
 
 @RestController
@@ -18,8 +21,19 @@ public class FlightRest {
         this.flightService = flightService;
     }
 
-    @GetMapping("flights")
-    public Flight getFlight(@RequestParam String flightNumber) {
-        return flightService.getFlight(flightNumber);
+    @PostMapping("api/citySuggestion")
+    public ResponseEntity<String[]> getSuggestions(@RequestParam String city) {
+        if (city == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(flightService.getSuggestions(city));
+    }
+
+    @PostMapping("api/flights")
+    public ResponseEntity<List<Flight>> getFlights(@RequestParam String from, @RequestParam String to, @RequestParam String date) {
+        if (from == null || to == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(flightService.getFlights(from, to, date));
     }
 }
--- src/main/java/server/service/FlightService.java
+++ src/main/java/server/service/FlightService.java
@@ -7,15 +7,12 @@ import java.util.List;
 
 @Service
 public class FlightService {
-    private List<Flight> flights;
 
-    public Flight getFlight(String number) {
-        for (Flight flight : flights) {
-            if (flight.getNumber().equals(number)) {
-                return flight;
-            }
-        }
-        //TODO: look up in web to find this flight and add to the service, if still dont exist, die or smth
+    public String[] getSuggestions(String city) {
+        return null;
+    }
+
+    public List<Flight> getFlights(String from, String to, String date) {
         return null;
     }
 }
