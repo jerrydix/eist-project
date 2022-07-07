96451dab1080ff38a38e7fc227d416fdd70ce86c
Lucio Sauer <lucio.sauer@posteo.net>
2022-07-07 17:00:14 +0200

    Add catering items

--- src/main/ui/src/views/Catering.vue
+++ src/main/ui/src/views/Catering.vue
@@ -4,6 +4,9 @@ const menuItems = [
 	{text: "NicNacs", price: 5.50},
 	{text: "Champagne 0.2L", price: 7.50},
 	{text: "Playboy Magazine Kids Edition", price: 6.90},
+	{text: "Nutella Jar plus Spoon", price: 2.49},
+	{text: "Leftovers from your neighbour", price: 0.99},
+	{text: "Water 0.5L", price: 2.35}
 ]
 
 
