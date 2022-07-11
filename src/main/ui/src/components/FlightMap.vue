909a988d1285174f1357e5a08f54a019411239c6
David Csida <david.csida@tum.de>
2022-07-11 14:54:35 +0200

    fixed reference error

---
+++ src/main/ui/src/components/FlightMap.vue
@@ -0,0 +1,23 @@
+<template>
+	<GoogleMap
+		:api-key="key"
+		style="width: 100%; height: 100%"
+		:center="center"
+		:zoom="10"
+	>
+		<Marker :options="{ position: center }" />
+		<Marker :options="{ position: excellence }" />
+	</GoogleMap>
+</template>
+
+<script setup>
+import { GoogleMap, Marker } from "vue3-google-map";
+
+const key = import.meta.env.VITE_GOOGLE_API_KEY;
+const center = { lat: 40.689247, lng: -74.044502 };
+const excellence = { lat: 48.2650433, lng: 11.6693806 };
+
+const props = defineProps({
+	flights: Array,
+});
+</script>
--- src/main/ui/src/views/Flights.vue
+++ src/main/ui/src/views/Flights.vue
@@ -1,96 +1,104 @@
 <template>
-  <w-flex class="xs6">
-    <h1>flights</h1>
-    <w-textarea v-model="this.departureCity" placeholder="Departure City" @input="searchDep"></w-textarea>
-    <w-list
-        v-model="selectedDeparture"
-        :items="departureSuggestions"
-        :multiple=false
-        :no-unselect=true
-        class="mt6 mr4 grow"
-        color="deep-purple"
-        @item-click="depClicked = $event">
-    </w-list>
-    <w-button @click="saveDep">Enter Departure Selection</w-button>
-    <br/>
-    <w-textarea v-model="this.arrivalCity" placeholder="Arrival City" @input="searchArr"></w-textarea>
-    <w-list
-        v-model="selectedArrival"
-        :items="arrivalSuggestions"
-        :multiple=false
-        :no-unselect=true
-        class="mt6 mr4 grow"
-        color="deep-purple"
-        @item-click="arrClicked = $event">
-    </w-list>
-    <w-button @click="saveArr">Enter Destination Selection</w-button>
-    <w-button @click="getFlights">Show Flights</w-button>
-    <FlightInfo v-if="this.flights"
-                :flight="flights[0]"
-    />
-  </w-flex>
+	<w-flex class="xs6">
+		<h1>flights</h1>
+		<w-textarea
+			v-model="this.departureCity"
+			placeholder="Departure City"
+			@input="searchDep"
+		></w-textarea>
+		<w-list
+			v-model="selectedDeparture"
+			:items="departureSuggestions"
+			:multiple="false"
+			:no-unselect="true"
+			class="mt6 mr4 grow"
+			color="deep-purple"
+			@item-click="depClicked = $event"
+		>
+		</w-list>
+		<w-button @click="saveDep">Enter Departure Selection</w-button>
+		<br />
+		<w-textarea
+			v-model="this.arrivalCity"
+			placeholder="Arrival City"
+			@input="searchArr"
+		></w-textarea>
+		<w-list
+			v-model="selectedArrival"
+			:items="arrivalSuggestions"
+			:multiple="false"
+			:no-unselect="true"
+			class="mt6 mr4 grow"
+			color="deep-purple"
+			@item-click="arrClicked = $event"
+		>
+		</w-list>
+		<w-button @click="saveArr">Enter Destination Selection</w-button>
+		<w-button @click="getFlights">Show Flights</w-button>
+		<FlightInfo v-if="this.flights" :flight="flights[0]" />
+	</w-flex>
 
-  <w-flex class="xs6">
-
-    <!-- <FlightMap></FlightMap> -->
-
-  </w-flex>
-
+	<w-flex class="xs6">
+		
+    <FlightMap :flight="this.flights"/>
+    
+	</w-flex>
 </template>
 
 <script>
-import {getFlights, getSuggestions} from "../services/FlightService.js";
+import { getFlights, getSuggestions } from "../services/FlightService.js";
 import FlightInfo from "../components/FlightInfo.vue";
+import FlightMap from "../components/FlightMap.vue";
 
 export default {
-  components: { FlightInfo, FlightMap },
-  data() {
-    return {
-      departureSuggestions: null,
-      selectedDeparture: null,
-      departureCity: null,
-      arrivalSuggestions: null,
-      selectedArrival: null,
-      arrivalCity: null,
-      depClicked: null,
-      arrClicked: null,
-      date: "12.07.2022",
-      flights: null
-    }
-  },
-  methods: {
-    searchDep(city) {
-      if (city.length > 2) {
-        getSuggestions(city).then((response) => {
-          this.departureSuggestions = response
-        });
-      }
-    },
-    searchArr(city) {
-      if (city.length > 2) {
-        getSuggestions(city).then((response) => {
-          this.arrivalSuggestions = response
-        });
-      }
-    },
-    saveDep() {
-      this.departureCity = this.selectedDeparture;
-      this.departureSuggestions = null
-    },
-    saveArr() {
-      this.arrivalCity = this.selectedArrival;
-      this.arrivalSuggestions = null
-    },
-    getFlights() {
-      getFlights(this.departureCity, this.arrivalCity, this.date).then((response) => {
-        this.flights = response
-        console.log(response)
-      });
-    }
-  }
-}</script>
+	components: { FlightInfo, FlightMap },
+	data() {
+		return {
+			departureSuggestions: null,
+			selectedDeparture: null,
+			departureCity: null,
+			arrivalSuggestions: null,
+			selectedArrival: null,
+			arrivalCity: null,
+			depClicked: null,
+			arrClicked: null,
+			date: "12.07.2022",
+			flights: null,
+		};
+	},
+	methods: {
+		searchDep(city) {
+			if (city.length > 2) {
+				getSuggestions(city).then((response) => {
+					this.departureSuggestions = response;
+				});
+			}
+		},
+		searchArr(city) {
+			if (city.length > 2) {
+				getSuggestions(city).then((response) => {
+					this.arrivalSuggestions = response;
+				});
+			}
+		},
+		saveDep() {
+			this.departureCity = this.selectedDeparture;
+			this.departureSuggestions = null;
+		},
+		saveArr() {
+			this.arrivalCity = this.selectedArrival;
+			this.arrivalSuggestions = null;
+		},
+		getFlights() {
+			getFlights(this.departureCity, this.arrivalCity, this.date).then(
+				(response) => {
+					this.flights = response;
+					console.log(response);
+				}
+			);
+		},
+	},
+};
+</script>
 
-<style>
-
-
-</style>
+<style></style>
