9c3c926396e5662aae8515937d84be612ae2dcb7
J3rry <ge58ler@mytum.de>
2022-07-15 17:24:13 +0200

    started user dashboard

--- src/main/ui/src/views/User/UserDashboard.vue
+++ src/main/ui/src/views/User/UserDashboard.vue
@@ -1,9 +1,19 @@
 <template>
-  <p>{{ this.user.username }}</p>
-  <p>{{ this.user.favouritePOIs }}</p>
-  <p>{{ this.user.rewards }}</p>
-  <p>{{ this.user.bookedFlightJourneys }}</p>
-  <FlightInfo :flight="this.user.currentFlight"/>
+  <div id="app">
+    <w-app>
+      <header>Dashboard</header>
+      <p>{{ this.user.username }}</p>
+      <p>{{ this.user.favouritePOIs }}</p>
+      <p>{{ this.user.rewards }}</p>
+      <p>{{ this.user.bookedFlightJourneys }}</p>
+
+      <w-flex grow>
+        <main class="grow">
+          <p class="reward">Reward: {{ this.user["latestReward"]["description"] }}</p>
+        </main>
+      </w-flex>
+    </w-app>
+  </div>
 </template>
 
 <script>
@@ -15,8 +25,29 @@ export default {
   components: {FlightInfo},
   data() {
     return {
-      user: null
-    };
+      user: null,
+      key: import.meta.env.VITE_GOOGLE_API_KEY,
+      map: true,
+      pois: [],
+      positions: [],
+      favourites: [],
+      favouritepositions: [],
+      current: {lat: 0, lng: 0},
+      curr: 0,
+      rewardTable: {
+        headers: [
+          {label: 'Reward', key: 'rewardType'},
+          {label: 'Reward', key: 'description'},
+        ],
+        items: [
+
+        ],
+        selectableRows: 1,
+        forceSelection: false,
+        selectableRowsOption: {label: '<code class="mr2">:selectable-row="false"</code> (default)', value: false},
+      },
+      selectionInfo: {}
+    }
   },
   setup() {
     const store = userStore();
@@ -33,4 +64,18 @@ export default {
 </script>
 
 
-<style scoped></style>
+<style scoped>
+
+.w-app {padding: 0px;background-color: #ffffff;}
+header, footer, aside, main {
+  margin: 0px;
+  padding: 0px;
+  color: #000000;
+  border: 0px solid rgba(0, 0, 0, 0.1);
+}
+header, footer {background-color: #ffffff; min-height: 4vh; align-content: center}
+aside {background-color: #ffffff}
+main {background-color: #ffffff;}
+
+</style>
+
