05b2ce1618468af7d2be3c6f97375c915820e5d9
J3rry <ge58ler@mytum.de>
2022-07-17 16:29:10 +0200

    Update FlightSuggestionCard.vue

--- src/main/ui/src/components/FlightSuggestionCard.vue
+++ src/main/ui/src/components/FlightSuggestionCard.vue
@@ -1,25 +1,18 @@
 <script>
-
 export default {
-
   props: ["flight"],
-
+}
 </script>
 
 
 <template>
-
   <w-card :title="this.flight.number">
-    <w-timeline
-        :items="[{title: this.flight.departureTime, content: this.flight.startName}, {title: this.flight.arrivalTime, content: this.flight.endName}]"
-        color="midnightblue"></w-timeline>
+    <w-timeline :items="[{title: this.flight.departureTime, content: this.flight.startName}, {title: this.flight.arrivalTime, content: this.flight.endName}]" color="primary" icon="wi-info-circle" ></w-timeline>
     <template #actions>
       <div class="spacer"></div>
       <w-button bg-color="success">
         Add to journey
       </w-button>
     </template>
-
   </w-card>
-
 </template>
