812b8959134eb90c4e2523c2cd08609f9660b37e
J3rry <ge58ler@mytum.de>
2022-07-17 16:09:38 +0200

    Update FlightSuggestionCard.vue

--- src/main/ui/src/components/FlightSuggestionCard.vue
+++ src/main/ui/src/components/FlightSuggestionCard.vue
@@ -1,13 +1,37 @@
-<script setup>
+<script>
 
+export default {
+  data() {
+    return {
+      flight : {
+        number : "PW1234",
+        startName : "Berlin",
+        endName : "Amsterdam",
+        departureTime : "12:00",
+        arrivalTime : "14:00",
+        departureDate : "12/12/2012"
+      },
+      items : {
+        {
+      }
+    }
+  }
+}
 </script>
 
 
 
 <template>
 
-    <w-card>
-        <p></p>
-    </w-card>
+  <w-card :title="this.flight.number">
+    <w-timeline :items="[{title: this.flight.departureTime, content: this.flight.startName}, {title: this.flight.arrivalTime, content: this.flight.endName}]" color="midnightblue"></w-timeline>
+    <template #actions>
+      <div class="spacer"></div>
+      <w-button bg-color="success">
+        Add to journey
+      </w-button>
+    </template>
+
+  </w-card>
 
 </template>
