19e80c13b1d1020c4489901d1970507fe8880442
GeorgeMer <georgemerezas@gmail.com>
2022-07-17 16:25:53 +0200

    Flights show suggestions

--- src/main/ui/src/components/FlightSuggestionCard.vue
+++ src/main/ui/src/components/FlightSuggestionCard.vue
@@ -1,30 +1,17 @@
 <script>
 
 export default {
-  data() {
-    return {
-      flight : {
-        number : "PW1234",
-        startName : "Berlin",
-        endName : "Amsterdam",
-        departureTime : "12:00",
-        arrivalTime : "14:00",
-        departureDate : "12/12/2012"
-      },
-      items : {
-        {
-      }
-    }
-  }
+  props: ["flight"],
 }
 </script>
-
 
 
 <template>
 
   <w-card :title="this.flight.number">
-    <w-timeline :items="[{title: this.flight.departureTime, content: this.flight.startName}, {title: this.flight.arrivalTime, content: this.flight.endName}]" color="midnightblue"></w-timeline>
+    <w-timeline
+        :items="[{title: this.flight.departureTime, content: this.flight.startName}, {title: this.flight.arrivalTime, content: this.flight.endName}]"
+        color="midnightblue"></w-timeline>
     <template #actions>
       <div class="spacer"></div>
       <w-button bg-color="success">
